494e61a747d6f046f0be1d8de60ccdae81c5499a
myrobotbit <41653060+myrobotbit@users.noreply.github.com>
2018-10-24 23:41:24 +0700

    Update MyServo.ts

--- MyServo.ts
+++ MyServo.ts
@@ -155,7 +155,7 @@ namespace MyServo {
     /**
      * Control Servo P0 to P12 set to keep pulse signal
      */
-    //% blockId="MyServo_ServoKeep" block="Servo keep %Servo |to %uSEC |µS"
+    //% blockId="MyServo_ServoKeep" block="Servo keep pulse %Servo|to %uSEC |µS"
     //% uSEC.min=0 uSEC.max=2500
     //% weight=80
     export function ServoKeep(ServoSelect:Servo, uSEC:number): void {
